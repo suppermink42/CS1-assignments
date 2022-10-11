098e2513f40e669378ce94ba148bbe05e698bce6
ayush4068 <ayush4068@gmail.com>
2022-10-10 20:35:44 -0700

    second

---
+++ assignment05B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+double fallingDistance(double);
+int main(){
+    for(int i = 0; i < 11; i++){
+        int display = fallingDistance(i);
+        cout << "The object falling for " << i << " time is " << display << " distance " << endl;
+    }
+}
+
+double fallingDistance(double time1){
+    int distance1 = 0.5 * 9.8 * time1 * time1;
+    return distance1;
+    //
+}
