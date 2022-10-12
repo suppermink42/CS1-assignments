268c67a5f5f3632257013d9f810a6aeb44d3f46a
suppermink42 <59209766+suppermink42@users.noreply.github.com>
2022-10-12 13:24:19 -0700

    updated

--- assignment05C.cpp
+++ assignment05C.cpp
@@ -1,63 +1,60 @@
 #include <iostream>
 using namespace std;
 
-void getScore(int &);
-void calcAverage(int &, int &, int &, int &, int &);
-int findLowest(int, int, int, int, int);
-int main(){
+void getScore(int&);
+void calcAverage(int&, int&, int&, int&, int&);
+int findLowest(int&, int&, int&, int&, int&);
+int main() {
     int scoreA;
     int scoreB;
     int scoreC;
     int scoreD;
     int scoreE;
-    
+
     getScore(scoreA);
     getScore(scoreB);
     getScore(scoreC);
     getScore(scoreD);
     getScore(scoreE);
 
-    cout << findLowest(scoreA, scoreB, scoreC, scoreD, scoreE) << endl;
+    findLowest(scoreA, scoreB, scoreC, scoreD, scoreE);
+    
+    calcAverage(scoreA, scoreB, scoreC, scoreD, scoreE);
 
 }
 
-void getScore(int &score){
+void getScore(int& score) {
     cout << "What is your score: " << endl;
     cin >> score;
-    while(score < 0 || score > 100){
+    while (score < 0 || score > 100) {
         cout << "Score between 1 - 100!" << endl;
         cin >> score;
     }
 };
 
-void calcAverage(int &score1, int &score2, int &score3, int &score4, int &score5){
+void calcAverage(int& score1, int& score2, int& score3, int& score4, int& score5) {
     double finalAvg = score1 + score2 + score3 + score4 + score5;
     double finalFinal = finalAvg / 4;
     cout << "The final average with the lowest score dropped: " << finalFinal << endl;
 };
 
 
-int findLowest(int score1A, int score1B, int score1C, int score1D, int score1E){
-    if(score1A < score1B && score1A < score1C && score1A < score1D && score1A < score1E){
+int findLowest(int &score1A, int &score1B, int &score1C, int &score1D, int &score1E) {
+    if (score1A < score1B && score1A < score1C && score1A < score1D && score1A < score1E) {
         score1A = 0;
-        calcAverage(score1A, score1B, score1C, score1D, score1E);
     }
-    else if(score1B < score1A && score1B < score1C && score1B < score1D && score1B < score1E){
+    else if (score1B < score1A && score1B < score1C && score1B < score1D && score1B < score1E) {
         score1B = 0;
-        calcAverage(score1A, score1B, score1C, score1D, score1E);
     }
-    else if(score1C < score1A && score1C < score1B && score1C < score1D && score1C < score1E){
+    else if (score1C < score1A && score1C < score1B && score1C < score1D && score1C < score1E) {
         score1C = 0;
-        calcAverage(score1A, score1B, score1C, score1D, score1E);
     }
-    else if(score1D < score1A && score1D < score1B && score1D < score1C && score1D < score1E){
+    else if (score1D < score1A && score1D < score1B && score1D < score1C && score1D < score1E) {
         score1D = 0;
-        calcAverage(score1A, score1B, score1C, score1D, score1E);
 
     }
-    else if(score1E < score1A && score1E < score1B && score1E < score1C && score1E < score1D){
+    else if (score1E < score1A && score1E < score1B && score1E < score1C && score1E < score1D) {
         score1E = 0;
-        calcAverage(score1A, score1B, score1C, score1D, score1E);
     }
     return 0;
 };
