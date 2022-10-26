ddc451acc12ec8d45ce569dc9fb74b9df6e37cc7
suppermink42 <59209766+suppermink42@users.noreply.github.com>
2022-10-25 20:55:17 -0700

    done

--- 6B.cpp
+++ 6B.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#define max 12
 using namespace std;
 
 
 int main(){
-    double dataSet[20];
-    double largest = 0;
-    double smallest = 0;
-    double total = 0;
-    double average = 0;
+    int dataSet[max];  
+    int counter = 0;
+    int val = 0;
+    
+    while(counter < 12){ 
+        cout << "Enter Value" << endl;
 
-    while(sizeof(dataSet) < 13){
-        
+        cin >> val;
+        if (val < 0){
+            cout << "Not negative numbers" << endl;
+
+        }
+        else{
+          dataSet[counter] = val;
+          counter++;
+
+        }       
 
     }
+   
+    
 }
